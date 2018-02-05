86015dce45f2101a0875bc97af45facc4f7d82ca
janmadelind <jamma0102@gmail.com>
2018-02-05 21:23:25 +0800

    New File

--- application/views/home/login.php
+++ application/views/home/login.php
@@ -1,8 +1,8 @@
-<div class="row">
-  <div class="container">
-    <div class="col-md-4">
-        <div class="well">
 
+<div class="row" style="background-image: url('<?php echo base_url();?>bootstrap2/images/slide_2.jpg'); height: 600px; width: 101%;">
+  <div class="container" >
+    <div class="col-md-5" style="margin: 200px 0 0 0">
+        <div class="well">
           <form class="" action="<?php echo base_url();?>Login/validate" method="post">
               <input type="text" name="username" class="form-control"placeholder="Username">
               <input type="password" name="password" class="form-control"placeholder="Password">
@@ -13,7 +13,7 @@
           </div>
 
           <div class="col-span-md-4">
-            <a href="">Create an Account</a>
+            <a href=" <?php echo base_url('Register');?> ">Create an Account</a>
           </div>
         </div>
 
---
+++ application/views/signup/signup.php
@@ -0,0 +1,38 @@
+
+<div class="row" style="background-image: url('<?php echo base_url();?>bootstrap2/images/slide_2.jpg'); height: 600px; width: 101%;">
+  <div class="container" >
+    <div class="col-md-6" style="margin: 200px 0 0 0">
+        <div class="row">
+          <div class="col-md-8">
+          </div>
+          <div class="col-md-6">
+            <div class="well">
+              <div class="container">
+                  <a href=" <?php echo base_url('Register/owner');?> "> Register Owner </a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+
+          </div>
+
+        </div>
+        <div class="row">
+          <div class="col-md-8">
+          </div>
+          <div class="col-md-6">
+            <div class="well">
+              <div class="container">
+                  <a href=" <?php echo base_url('Register/tenant');?>"> Register Tenant </a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+
+          </div>
+
+        </div>
+
+    </div>
+  </div>
+</div>
