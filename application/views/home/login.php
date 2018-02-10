95e96aea3f5f5fa25d7920796c24dadf62e05f52
carloperalta <carlorap0613@gmail.com>
2018-02-11 01:55:01 +0800

    Cleans the Project

--- application/views/home/login.php
+++ application/views/home/login.php
@@ -1,36 +1,32 @@
+<div class="row" style="background-image: url('<?php echo base_url();?>assets/images/new2.jpg');background-attachment:fixed; height:100%; margin:0">
 
-<div class="row" style="background-image: url('<?php echo base_url();?>bootstrap2/images/slide_21.jpg');background-size: 100% 100%"> 
-  <div class="container" >
-    <div class="col-md-5" style="margin: 200px 0 0 0">
+  <div class="row" style="height: 100%;margin: 100px 0 0 0">
+    <div class="col-md-4">
+      
+    </div>
+
+    <div class="col-md-4">
+      <div class="row">
+        
         <div class="well">
           <h2 style="text-align: center;">Welcome to Rental Finder</h2>
 
           <?php
-              if ($this->session->flashdata('success')) {
-                echo '<div class="alert alert-success">'.$this->session->flashdata('success').'</div>';
-              }else if ($this->session->flashdata('error')) {
+              if ($this->session->flashdata('error')) {
                 echo '<div class="alert alert-danger">'.$this->session->flashdata('error').'</div>';
               }
             ?>
 
           <form class="" action="" method="post">
-            <div class="col-md-4">
-              
-            </div>
               <input type="text" name="username" class="form-control" placeholder="Username" id="username" required="">
               <input type="password" name="password" class="form-control" placeholder="Password" id="password" required=""> 
               <br>
               <a href=" <?php echo base_url('Register');?>" >Create an Account</a>
-              <button class="btn btn-primary" style="margin-right:0;float:right;">Log In</button>
-
+              <button class="btn btn-primary" style="margin-right:0; float:right;">Log In</button>
           </form>
-          <div class="col-md-4">
-
-          </div>
-          <br>
-          <div class="col-span-md-4">
-          </div>
         </div>
+      </div>
+
 
     </div>
   </div>
---
+++ application/views/owner/dashboard.php
@@ -0,0 +1,153 @@
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="card">
+                            <div class="content">
+                                <div class="row">
+                                    <div class="col-xs-5">
+                                        <div class="icon-big icon-warning text-center">
+                                            <i class="ti-server"></i>
+                                        </div>
+                                    </div>
+                                    <div class="col-xs-7">
+                                        <div class="numbers">
+                                           <p>Property Capacity</p>
+                                            5/Room
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="footer">
+                                    <hr />
+                                    <div class="stats">
+                                        <i class="ti-reload"></i> Updated now
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="card">
+                            <div class="content">
+                                <div class="row">
+                                    <div class="col-xs-5">
+                                        <div class="icon-big icon-success text-center">
+                                            <i class="ti-wallet"></i>
+                                        </div>
+                                    </div>
+                                    <div class="col-xs-7">
+                                        <div class="numbers">
+                                            <p>Rates (Per person)</p>
+                                            ₱1,500
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="footer">
+                                    <hr />
+                                    <div class="stats">
+                                        <i class="ti-calendar"></i> Last day
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="card">
+                            <div class="content">
+                                <div class="row">
+                                    <div class="col-xs-5">
+                                        <div class="icon-big icon-danger text-center">
+                                            <i class="ti-pulse"></i>
+                                        </div>
+                                    </div>
+                                    <div class="col-xs-7">
+                                        <div class="numbers">
+                                           <p>Room Status</p>
+                                            2 vacant room
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="footer">
+                                    <hr />
+                                    <div class="stats">
+                                        <i class="ti-timer"></i> In the last hour
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="header">
+                                <h4 class="title">Users Behavior</h4>
+                                <p class="category">24 Hours performance</p>
+                            </div>
+                            <div class="content">
+                                <div id="chartHours" class="ct-chart"></div>
+                                <div class="footer">
+                                    <div class="chart-legend">
+                                        <i class="fa fa-circle text-info"></i> Open
+                                        <i class="fa fa-circle text-danger"></i> Click
+                                        <i class="fa fa-circle text-warning"></i> Click Second Time
+                                    </div>
+                                    <hr>
+                                    <div class="stats">
+                                        <i class="ti-reload"></i> Updated 3 minutes ago
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="card">
+                            <div class="header">
+                                <h4 class="title">Email Statistics</h4>
+                                <p class="category">Last Campaign Performance</p>
+                            </div>
+                            <div class="content">
+                                <div id="chartPreferences" class="ct-chart ct-perfect-fourth"></div>
+
+                                  <div class="footer">
+                                    <div class="chart-legend">
+                                        <i class="fa fa-circle text-info"></i> Occupied Room
+                                        <i class="fa fa-circle text-danger"></i> Reserved Room
+                                        <i class="fa fa-circle text-warning"></i> Vacant
+                                    </div>
+                                    <hr>
+                                    <div class="stats">
+                                         <i class="ti-timer"></i> RentalFinder sent 2 days ago
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="card ">
+                            <div class="header">
+                                <h4 class="title">2015 Sales</h4>
+                                <p class="category">All Rental Property including Taxes</p>
+                            </div>
+                            <div class="content">
+                                <div id="chartActivity" class="ct-chart"></div>
+
+                                <div class="footer">
+                                    <div class="chart-legend">
+                                         <i class="fa fa-circle text-info"></i> Condominium
+                                        <i class="fa fa-circle text-warning"></i> Dormitory
+                                    </div>
+                                    <hr>
+                                    <div class="stats">
+                                        <i class="ti-check"></i> Data information certified
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        
